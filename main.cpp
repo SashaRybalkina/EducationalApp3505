1b39ac3de0093fc2129e9f61a76a0c3c99f7ed36
BrendonGor <brendon.t.gorelik@gmail.com>
2023-12-08 20:37:02 -0700

    Added Background
    Set in main

--- main.cpp
+++ main.cpp
@@ -13,10 +13,18 @@ int main(int argc, char *argv[])
     QRect screenGeometry = screen->geometry();
     int width = screenGeometry.width();
     int height = screenGeometry.height();
-    qDebug() << width;
+    // create interactions and world
     std::array<std::tuple<std::string, int>, 4> interactions = {std::make_tuple("discussion", -2), std::make_tuple("slander", 1), std::make_tuple("fight", -2), std::make_tuple("shake hands", 1)};
     World world(width, height, interactions);
     MainWindow w(world);
+    // get background
+    QPixmap background(":/background.jpg");
+    // Resize image to fit the window dimensions
+    background = background.scaled(screen->size(), Qt::IgnoreAspectRatio);
+    // Set the background
+    QPalette palette;
+    palette.setBrush(QPalette::Window, background);
+    w.setPalette(palette);
     w.show();
     return a.exec();
 }
